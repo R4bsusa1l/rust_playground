65c5390993c7fedff92fc294740b6db889ae6009
R4bsusail <silasbaur0@gmail.com>
2025-04-24 12:46:00 +0200

    chapter 11.2

---
+++ adder/target/debug/deps/adder-3cf2d91ec037dc14.d
@@ -0,0 +1,5 @@
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\adder\target\debug\deps\libadder-3cf2d91ec037dc14.rmeta: src\lib.rs
+
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\adder\target\debug\deps\adder-3cf2d91ec037dc14.d: src\lib.rs
+
+src\lib.rs:
