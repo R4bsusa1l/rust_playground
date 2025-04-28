10bc3f1603578a1ee4566cab5e9dc7147fa9db8f
R4bsusail <silasbaur0@gmail.com>
2025-04-28 11:48:32 +0200

    chapter 12.4

---
+++ minigrep/target/debug/deps/minigrep-10889edee0ee8d76.d
@@ -0,0 +1,5 @@
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\minigrep\target\debug\deps\minigrep-10889edee0ee8d76.exe: src\main.rs
+
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\minigrep\target\debug\deps\minigrep-10889edee0ee8d76.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/deps/minigrep-b9f93443feb600d6.d
@@ -0,0 +1,5 @@
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\minigrep\target\debug\deps\libminigrep-b9f93443feb600d6.rmeta: src\main.rs
+
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\minigrep\target\debug\deps\minigrep-b9f93443feb600d6.d: src\main.rs
+
+src\main.rs:
