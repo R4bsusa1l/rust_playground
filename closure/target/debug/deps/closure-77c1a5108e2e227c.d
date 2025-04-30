0c840cbd7e220c758d692292c6fd8b4cd7569ca4
R4bsusail <silasbaur0@gmail.com>
2025-04-30 09:00:13 +0200

    chapter 13.1

---
+++ closure/target/debug/deps/closure-77c1a5108e2e227c.d
@@ -0,0 +1,5 @@
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\closure\target\debug\deps\libclosure-77c1a5108e2e227c.rmeta: src\main.rs
+
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\closure\target\debug\deps\closure-77c1a5108e2e227c.d: src\main.rs
+
+src\main.rs:
---
+++ closure/target/debug/deps/closure-c6ebb8879baa0e2e.d
@@ -0,0 +1,5 @@
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\closure\target\debug\deps\libclosure-c6ebb8879baa0e2e.rmeta: src\lib.rs
+
+C:\Users\silas\OneDrive - ZHAW\Desktop\GitTransfer\rust_playground\closure\target\debug\deps\closure-c6ebb8879baa0e2e.d: src\lib.rs
+
+src\lib.rs:
